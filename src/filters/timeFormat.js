f3ea6064fd6c7c9aca9b06d9bef622a84f7e557a
wenli <a95916127128@163.com>
2017-09-11 03:15:27 +0800

    vue-news for app

---
+++ src/filters/timeFormat.js
@@ -0,0 +1,16 @@
+export const normalTime = function (time)  {
+    if (time) {
+        var oDate = new Date();
+        oDate.setTime(time);
+
+        var y = oDate.getFullYear();
+        var m = oDate.getMonth() + 1;
+        var d = oDate.getDate();
+
+        var h = oDate.getHours();
+        var m = oDate.getMinutes();
+        var s = oDate.getSeconds();
+
+        return y + '-' + m + '-' + d + ' ' + h + ':' + m + ':' + s;
+    }
+}
